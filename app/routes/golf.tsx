af83c84f13a3fc0cec418b8fdf234fe9a99385f1
Gary Varner <garysarahvarner@gmail.com>
2025-02-09 22:27:19 -0500

    Adding table and espn calls

--- app/routes/golf.tsx
+++ app/routes/golf.tsx
@@ -1,17 +1,68 @@
 import { Link, Outlet, createFileRoute } from "@tanstack/react-router";
-import { getLeaderboard } from "../utils/espn";
+import { getScoreboard } from "../utils/espn";
 
 export const Route = createFileRoute("/golf")({
-  loader: async () => getLeaderboard(),
+  loader: async () => getScoreboard(),
   component: LeaderBoardComponent,
 });
 
 function LeaderBoardComponent() {
   const event = Route.useLoaderData();
+  const players = event.competitions[0].competitors.slice(0, 20);
 
   return (
-    <div className="p-2 flex gap-2">
-      <h1>{event.name}</h1>
+    <div className="p-2 m-auto w-1/2">
+      <div>
+        <h1>{event.name}</h1>
+      </div>
+      <div className="relative w-full overflow-auto">
+        <table className="w-full caption-bottom text-sm">
+          <thead>
+            <tr>
+              <th className="h-12 px-4 text-left align-middle font-medium text-muted-foreground">
+                Player
+              </th>
+              <th className="h-12 px-4 text-left align-middle font-medium text-muted-foreground">
+                Score
+              </th>
+              <th className="h-12 px-4 text-left align-middle font-medium text-muted-foreground">
+                R1
+              </th>
+              <th className="h-12 px-4 text-left align-middle font-medium text-muted-foreground">
+                R2
+              </th>
+              <th className="h-12 px-4 text-left align-middle font-medium text-muted-foreground">
+                R3
+              </th>
+              <th className="h-12 px-4 text-left align-middle font-medium text-muted-foreground">
+                R4
+              </th>
+            </tr>
+          </thead>
+          {players.map((player) => {
+            return (
+              <tr key={player.id} className="p-2">
+                <td className="p-2 align-middle">
+                  <div className="flex">
+                    <div>
+                      <img
+                        src={player.athlete.flag.href}
+                        className="w-6 h-6 rounded-full"
+                      ></img>
+                    </div>
+                    <div>{player.athlete.displayName}</div>
+                  </div>
+                </td>
+                <td>{player.score}</td>
+                <td>{player.linescores[0].value}</td>
+                <td>{player.linescores[1].value}</td>
+                <td>{player.linescores[2].value}</td>
+                <td>{player.linescores[3].value}</td>
+              </tr>
+            );
+          })}
+        </table>
+      </div>
     </div>
   );
 }
--- app/utils/espn.tsx
+++ app/utils/espn.tsx
@@ -11,3 +11,32 @@ export async function getLeaderboard(): Promise<{}> {
 
   return events[0];
 }
+
+// gets pga golf on thur-sun
+export async function getScoreboard(): Promise<{}> {
+  const res = await fetch(
+    "https://site.api.espn.com/apis/site/v2/sports/golf/pga/scoreboard"
+  );
+
+  if (!res.ok) {
+    throw new Error(`Failed to fetch today's board: ${res.statusText}`);
+  }
+
+  const { events } = await res.json();
+
+  return events[0];
+}
+
+export async function getEventPlayers(): Promise<{}> {
+  const res = await fetch(
+    "https://site.web.api.espn.com/apis/site/v2/sports/golf/pga/leaderboard/players?region=us&lang=en&event=401703494"
+  );
+
+  if (!res.ok) {
+    throw new Error(`Failed to fetch today's board: ${res.statusText}`);
+  }
+
+  const { leaderboard } = await res.json();
+
+  return leaderboard;
+}
