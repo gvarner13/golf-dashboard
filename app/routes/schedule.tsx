66daed43c8d8dfdaccafb3010c247336ab1e40f9
Gary Varner <garysarahvarner@gmail.com>
2025-02-13 21:48:18 -0500

    adding shadcn to make it look better

--- app/routes/schedule.tsx
+++ app/routes/schedule.tsx
@@ -1,5 +1,13 @@
 import { Link, Outlet, createFileRoute } from "@tanstack/react-router";
 import { getTourSchedule, EventData } from "../utils/espn";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export const Route = createFileRoute("/schedule")({
   loader: async () => getTourSchedule(),
@@ -10,9 +18,26 @@ function TourEventsComponent() {
   const events = Route.useLoaderData();
 
   return (
-    <div>
+    <div className="p-2 m-auto w-1/3">
       {events.map((event: EventData) => {
-        return <div key={event.id}>{event.label}</div>;
+        return (
+          <Card key={event.id} className="mb-2">
+            <CardContent>
+              <div className="flex justify-between pt-6">
+                <div>
+                  <div>{event.label}</div>
+                  <div>{event.locations[0]}</div>
+                  <div>{event.winner?.competitors?.displayName || "N/A"}</div>
+                  <div>{event.defendingChampion?.displayName || "N/A"}</div>
+                </div>
+                <div>
+                  <div>{event.detail}</div>
+                  <div>{event.purse?.displayValue}</div>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+        );
       })}
     </div>
   );
