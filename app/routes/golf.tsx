7a5297c3b1c717b97dc7b563c6edba01e8147e8b
Gary Varner <garysarahvarner@gmail.com>
2025-02-17 07:09:08 -0500

    adding star svg for fave players

--- app/routes/golf.tsx
+++ app/routes/golf.tsx
@@ -15,6 +15,25 @@ export const Route = createFileRoute("/golf")({
   component: LeaderBoardComponent,
 });
 
+import type { SVGProps } from "react";
+
+export function LightStarRounded(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        // fill="currentColor"
+        d="m12 16.102l-3.63 2.192q-.16.079-.297.064q-.136-.016-.265-.094q-.13-.08-.196-.226t-.012-.319l.966-4.11l-3.195-2.77q-.135-.11-.178-.263t.019-.293t.165-.23q.104-.087.28-.118l4.216-.368l1.644-3.892q.068-.165.196-.238T12 5.364t.288.073t.195.238l1.644 3.892l4.215.368q.177.03.281.119q.104.088.166.229q.061.14.018.293t-.178.263l-3.195 2.77l.966 4.11q.056.171-.011.318t-.197.226q-.128.08-.265.095q-.136.015-.296-.064z"
+      ></path>
+    </svg>
+  );
+}
+
 function LeaderBoardComponent() {
   const event = Route.useLoaderData();
   const players: PlayerData[] = event.competitions[0].competitors.slice(0, 20);
@@ -29,6 +48,7 @@ function LeaderBoardComponent() {
           <Table>
             <TableHeader>
               <TableRow>
+                <TableHead>Pos</TableHead>
                 <TableHead>Player</TableHead>
                 <TableHead>Score</TableHead>
                 <TableHead>R1</TableHead>
@@ -41,6 +61,7 @@ function LeaderBoardComponent() {
               {players.map((player) => {
                 return (
                   <TableRow key={player.id}>
+                    <TableCell>{player.order}</TableCell>
                     <TableCell>
                       <div className="flex">
                         <div>
@@ -50,6 +71,9 @@ function LeaderBoardComponent() {
                           ></img>
                         </div>
                         <div className="ml-1">{player.athlete.displayName}</div>
+                        <div>
+                          <LightStarRounded className="fill-gray-400" />
+                        </div>
                       </div>
                     </TableCell>
                     <TableCell>{player.score}</TableCell>
